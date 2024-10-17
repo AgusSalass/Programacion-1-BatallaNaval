2aadd631a1e1d120a9a003d7aff02937108edb09
nragagnin <nicolasragagnin@gmail.com>
2024-10-17 14:40:18 -0300

    modificacion funcion deteccion de disparos en archivo de prueba

--- Modulo_funciones/xdddd.py
+++ Modulo_funciones/xdddd.py
@@ -1,6 +1,32 @@
-barcosj1 = [[(1,1),(1,2),(1,3)], [(1,1),(1,2)], [], [], []]
+barcosj1 = [[(1,1),(1,2),(1,3),(1,4),(1,5)],[(1,1),(1,2),(1,3),(1,4)],[(1,1),(1,2),(1,3)],[(1,1),(1,2),(1,3)],[(1,1),(1,2)]]
 
-def deteccion_hits(hit, lista_barcos):
+o = (f"\033[36m ~\033[0m")
+b = (f"\033[90m ≡\033[0m")
+portaaviones = b
+mulportaaviones = 5
+destructor = b
+muldestructor = 4
+crucero1 = b
+mulcrucero1= 3
+crucero2 = b
+mulcrucero2 = 3
+lancha = b
+mullancha = 2
+
+j1_tablerodisparos= [["╔","══","══","══","══","══","══","══","══","══","══","═╗","portaaviones: ",portaaviones*mulportaaviones],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║","destructor: ",destructor*muldestructor],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║","crucero: ",crucero1*mulcrucero1],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║","crucero: ",crucero2*mulcrucero2],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║","lancha: ", lancha*mullancha],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["║",o,o,o,o,o,o,o,o,o,o," ║"],
+                        ["╠","══","══","══","══","══","══","══","══","══","══","═╣"]]
+
+def deteccion_hits(hit, lista_barcos, tablero, xportaaviones, xdestructor, xcrucero1, xcrucero2, xlancha):
     i = 0
     encontrado = False
     while i < len(lista_barcos) and encontrado == False:
@@ -9,14 +35,37 @@ def deteccion_hits(hit, lista_barcos):
             if hit == lista_barcos[i][j] and encontrado == False:
                 del lista_barcos[i][j]
                 encontrado = True
-                if not lista_barcos[i]:
-                    del lista_barcos[i]
+                if lista_barcos[i] == []:
+                    #hardcodeado, ver como evitar
+                    if i == 0:
+                        tablero[i][13] = (f"\033[31mDestruido\033[0m")
+                        xportaaviones = 1
+                    elif i == 1:
+                        tablero[i][13] = (f"\033[31mDestruido\033[0m")
+                        xdestructor = 1
+                    elif i == 2:
+                        tablero[i][13] = (f"\033[31mDestruido\033[0m")
+                        xcrucero1 = 1
+                    elif i == 3:
+                        tablero[i][13] = ( f"\033[31mDestruido\033[0m")
+                        xcrucero2 = 1
+                    elif i == 4:
+                        tablero[i][13] = (f"\033[31mDestruido\033[0m")
+                        xlancha = 1
             j += 1
         i += 1
+    return xportaaviones, xdestructor, xcrucero1, xcrucero2, xlancha
 
-for i in range(3):
+def dibujar(tablero):
+    for fila in tablero:
+        for columna in fila:
+            print(columna,end="")
+        print()
+
+while True:
     x=int(input("x"))
     y=int(input("y"))
     pos_bomba=(x,y)
-    deteccion_hits(pos_bomba, barcosj1)
+    mulportaaviones, muldestructor, mulcrucero1, mulcrucero2, mullancha = deteccion_hits(pos_bomba, barcosj1, j1_tablerodisparos, mulportaaviones, muldestructor, mulcrucero1, mulcrucero2, mullancha)
     print(barcosj1)
+    dibujar(j1_tablerodisparos)
