13d667ce251c089dc9c354ab06bb8af013dafb6f
nragagnin <nicolasragagnin@gmail.com>
2024-10-23 11:55:25 -0300

    arregle bugs en modulo usuarios y converti en funcion

--- Modulo_funciones/UsuariosP.py
+++ Modulo_funciones/UsuariosP.py
@@ -2,46 +2,60 @@ import json
 import os
 import re
 
-path_archivo = os.path.dirname(os.path.abspath(__file__))
+def sign_up(usuario):
+	path_archivo = os.path.dirname(os.path.abspath(__file__))
+	try:
+		arch_div = os.path.join(path_archivo, f"Usuarios.json")
+		contenido = open(arch_div,"r")
+		cont_existente = contenido.read()
+		contenido.close()
+		diccionario_usuarios = json.loads(cont_existente)
 
-nuevo_usuario = str(input("usuario (maximo 12 caracteres, solo letras y numeros): "))
-while not re.match("[a-zA-Z0-9]{5,12}", nuevo_usuario):
-	nuevo_usuario = str(input("usuario invalido, ingrese uno nuevo (maximo 12 caracteres, solo letras y numeros): "))
-nueva_contrasena = str(input("contraseña: "))
-while not re.match("[a-zA-Z0-9]{5,12}", nueva_contrasena):
-    nueva_contrasena = str(input("contraseña invalida, ingrese una nueva (maximo 12 caracteres, solo letras y numeros): "))
+		if usuario in diccionario_usuarios:
+			while usuario in diccionario_usuarios:
+				print("usuario ya existe")
+				usuario = str(input("usuario (maximo 12 caracteres, solo letras y numeros): "))
+				while not re.match("^[a-zA-Z0-9]{5,12}$", usuario):
+					usuario = str(input("usuario invalido, ingrese uno nuevo (maximo 12 caracteres, solo letras y numeros): "))
+		
+		contrasena = str(input("contraseña: "))
+		while not re.match("^[a-zA-Z0-9]{5,12}$", contrasena):
+			contrasena = str(input("contraseña invalida, ingrese una nueva (maximo 12 caracteres, solo letras y numeros): "))
 
-try:
-	arch_div = os.path.join(path_archivo, f"Usuarios.json")
-	contenido = open(arch_div,"r")
-	cont_existente = contenido.read()
-	contenido.close()
-	usuarios = json.loads(cont_existente)
-
-	while nuevo_usuario in usuarios:
-		print("usuario ya existe")
-		nuevo_usuario = str(input("usuario (maximo 12 caracteres, solo letras y numeros): "))
-		while not re.match("[a-zA-Z0-9]{12}", nuevo_usuario):
-			nuevo_usuario = str(input("usuario invalido, ingrese uno nuevo (maximo 12 caracteres, solo letras y numeros): "))
-		nueva_contrasena = str(input("contraseña: "))
-		while not re.match("[a-zA-Z0-9]{12}", nueva_contrasena):
-			nueva_contrasena = str(input("contraseña invalida, ingrese una nueva (maximo 12 caracteres, solo letras y numeros): "))
-	else:
-		usuarios[nuevo_usuario] = {
-			"contrasena": nueva_contrasena,
+		diccionario_usuarios[usuario] = {
+			"contrasena": contrasena,
 			"puntaje": 0
 		}
+		usuarios_JSON = json.dumps(diccionario_usuarios, indent=4)
 
-		usuarios_JSON = json.dumps(usuarios, indent=4)
 		try:
 			contenido = open(arch_div, "w")
 			contenido.write(usuarios_JSON)
 			contenido.close()
+
 		except TypeError:
 			print(TypeError)
 			print("error de grabado de cambios")
-			
 
-except TypeError:
-    print(TypeError)
-    print("error de lectura:")
+	except TypeError:
+		print(TypeError)
+		print("error de lectura:")
+
+def log_in():
+	pass
+
+#programa principal
+repetir = True
+while repetir:
+	os.system("cls")
+	print ("6-Sign Up")
+	print ("7-Log In")
+	op=int(input("Eliga una opcion: "))
+	if op == 6:
+		nuevo_usuario = str(input("usuario (maximo 12 caracteres, solo letras y numeros): "))
+		while not re.match("^[a-zA-Z0-9]{5,12}$", nuevo_usuario):
+			nuevo_usuario = str(input("usuario invalido, ingrese uno nuevo (maximo 12 caracteres, solo letras y numeros): "))
+		sign_up(nuevo_usuario)
+
+	elif op == 7:
+		log_in()
