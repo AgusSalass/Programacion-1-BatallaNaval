b69e36d72a431637b6acf03e79dc7b1f9bad7892
Ezequiel <gezequield@gmail.com>
2024-11-03 15:29:47 -0300

    Revert "Ezequiel Diaz"
    This reverts commit eba4247b172cf449c929766dffe97cfa62b1d570.

--- Modulo_funciones/Core.py
+++ Modulo_funciones/Core.py
@@ -5,6 +5,74 @@ import copy
 import cursor
 import json
 import sys
+import re
+
+def leer_archivo():
+	try:
+		path_archivo = os.path.dirname(os.path.abspath(__file__))
+		arch_dic = os.path.join(path_archivo, f"Usuarios.json")
+		contenido = open(arch_dic,"r")
+		cont_existente = contenido.read()
+		contenido.close()
+		diccionario_usuarios = json.loads(cont_existente)
+		return diccionario_usuarios
+	except TypeError:
+		print(TypeError)
+		print("Error de lectura")
+
+def log_in(usuario, diccionario_usuarios):
+	bucle = True
+	if usuario in diccionario_usuarios:
+		contrasena = str(input("Ingrese su contraseña (5-12 Caracteres, solo letras y numeros), o -1 para volver a menu: "))
+		if contrasena == "-1":
+			bucle = -1
+		while contrasena != diccionario_usuarios[usuario]["contrasena"] and bucle != -1:
+			os.system("cls")
+			print("Contrasena incorrecta")
+			contrasena = str(input("Ingrese su contraseña (5-12 Caracteres, solo letras y numeros), o -1 para volver a menu: "))
+			if contrasena == "-1":
+				bucle = -1
+		if contrasena != "-1":
+			print("Bienvenido", usuario, ". Presione 'Enter' para continuar: ")
+			input()
+	else:
+		print("Nombre de usuario no existente. Presione 'Enter' para continuar: ")
+		input()
+
+def sign_up(usuario, diccionario_usuarios):
+	if usuario in diccionario_usuarios:
+		while usuario in diccionario_usuarios:
+			os.system("cls")
+			print("Nombre de usuario ya existente")
+			usuario = str(input("Ingrese un nombre de usuario (5-12 Caracteres, solo letras y numeros): "))
+			while not re.match("^[a-zA-Z0-9]{5,12}$", usuario):
+				os.system("cls")
+				print("Nombre de usuario invalido")
+				usuario = str(input("Ingrese un nombre de usuario (5-12 Caracteres, solo letras y numeros): "))
+	
+	contrasena = str(input("Ingrese una contraseña (5-12 Caracteres, solo letras y numeros): "))
+	while not re.match("^[a-zA-Z0-9]{5,12}$", contrasena):
+		os.system("cls")
+		print("Contraseña invalida")
+		contrasena = str(input("Ingrese una contraseña (5-12 Caracteres, solo letras y numeros): "))
+
+	diccionario_usuarios[usuario] = {
+		"contrasena": contrasena,
+		"puntaje": 0
+	}
+	usuarios_JSON = json.dumps(diccionario_usuarios, indent=4)
+	try:
+		path_archivo = os.path.dirname(os.path.abspath(__file__))
+		arch_dic = os.path.join(path_archivo, f"Usuarios.json")
+		contenido = open(arch_dic, "w")
+		contenido.write(usuarios_JSON)
+		contenido.close()
+		print("Bienvenido", usuario, ". Presione 'Enter' para continuar: ")
+		input()
+	except TypeError:
+		print(TypeError)
+		print("Error de grabado")
+
 
 def mostrar_equipo():
     os.system("cls")
@@ -49,25 +117,47 @@ def menu():
         clock.tick(24)
         os.system("cls")
         if op == 0:
-            print("\033[16;60H\033[104m  Juego    \033[0m")
-            print("\033[17;60H  Equipo   ")
-            print("\033[18;60H Proyecto  ")
-            print("\033[19;60H  Salir    ")
+            print("\033[16;60H\033[104m     Juego     \033[0m")
+            print("\033[17;60H Iniciar Sesión ")
+            print("\033[18;60H  Registrarse  ")
+            print("\033[19;60H     Equipo     ")
+            print("\033[20;60H    Proyecto    ")
+            print("\033[21;60H     Salir     ")
         elif op == 1:
-            print("\033[16;60H  Juego    ")
-            print("\033[17;60H\033[104m  Equipo   \033[0m")
-            print("\033[18;60H Proyecto  ")
-            print("\033[19;60H  Salir    ")
+            print("\033[16;60H     Juego     ")
+            print("\033[17;60H\033[104m Iniciar Sesión \033[0m")
+            print("\033[18;60H  Registrarse  ")
+            print("\033[19;60H     Equipo     ")
+            print("\033[20;60H    Proyecto    ")
+            print("\033[21;60H     Salir     ")
         elif op == 2:
-            print("\033[16;60H  Juego    ")
-            print("\033[17;60H  Equipo   ")
-            print("\033[18;60H\033[104m Proyecto  \033[0m")
-            print("\033[19;60H  Salir    ")
+            print("\033[16;60H     Juego     ")
+            print("\033[17;60H Iniciar Sesión ")
+            print("\033[18;60H\033[104m  Registrarse  \033[0m")
+            print("\033[19;60H     Equipo     ")
+            print("\033[20;60H    Proyecto    ")
+            print("\033[21;60H     Salir     ")
         elif op == 3:
-            print("\033[16;60H  Juego    ")
-            print("\033[17;60H  Equipo   ")
-            print("\033[18;60H Proyecto  ")
-            print("\033[19;60H\033[104m  Salir    \033[0m")
+            print("\033[16;60H     Juego     ")
+            print("\033[17;60H Iniciar Sesión ")
+            print("\033[18;60H  Registrarse  ")
+            print("\033[19;60H\033[104m     Equipo     \033[0m")
+            print("\033[20;60H    Proyecto    ")
+            print("\033[21;60H     Salir     ")
+        elif op == 4:
+            print("\033[16;60H     Juego     ")
+            print("\033[17;60H Iniciar Sesión ")
+            print("\033[18;60H  Registrarse  ")
+            print("\033[19;60H     Equipo     ")
+            print("\033[20;60H\033[104m    Proyecto    \033[0m")
+            print("\033[21;60H     Salir     ")
+        elif op == 5:
+            print("\033[16;60H     Juego     ")
+            print("\033[17;60H Iniciar Sesión ")
+            print("\033[18;60H  Registrarse  ")
+            print("\033[19;60H     Equipo     ")
+            print("\033[20;60H    Proyecto    ")
+            print("\033[21;60H\033[104m     Salir     \033[0m")
         if keyboard.is_pressed('w'):
             if presionado == False:
                 if op-1 != -1:
@@ -75,7 +165,7 @@ def menu():
             presionado = True
         elif keyboard.is_pressed('s'):
             if presionado == False:
-                if op+1 != 4:
+                if op+1 != 6:
                     op += 1
             presionado = True
         elif keyboard.is_pressed('e'):
@@ -84,12 +174,31 @@ def menu():
                     repetir = False
                     juego()
                 elif op == 1:
-                    repetir = False
-                    mostrar_equipo()
+                    os.system("cls")
+                    input("Presione 'Enter' para continuar: ")
+                    nuevo_usuario = str(input("Ingrese su nombre de usuario (5-12 Caracteres, solo letras y numeros): "))
+                    usuarios = leer_archivo()
+                    log_in(nuevo_usuario, usuarios)    
                 elif op == 2:
-                    repetir = False
-                    mostrar_proyecto()
+                    os.system("cls")
+                    input("Presione 'Enter' para continuar: ")
+                    nuevo_usuario = str(input("Ingrese un nombre de usuario (5-12 Caracteres, solo letras y numeros): "))
+                    while not re.match("^[a-zA-Z0-9]{5,12}$", nuevo_usuario):
+                        os.system("cls")
+                        print("Nombre de usuario invalido")
+                        nuevo_usuario = str(input("Ingrese un nombre de usuario (5-12 Caracteres, solo letras y numeros): "))
+                    usuarios = leer_archivo()
+                    sign_up(nuevo_usuario, usuarios)
                 elif op == 3:
+                    mostrar_equipo()
+                    print()
+                    input("Presione 'Enter' para continuar: ")
+                elif op == 4:
+                    mostrar_proyecto()
+                    print()
+                    input("Presione 'Enter' para continuar: ")
+                elif op == 5:
+                    os.system("cls")
                     repetir = False
             presionado = True
         else:
@@ -100,8 +209,13 @@ def dibujar(tablero):
             print(columna,end="")
         print()
         
-def generar_tablero():
-    pass
+def convertir_a_numero(tablero):
+    for i in range(len(tablero)):
+        for j in range(len(tablero[i])):
+            if  "≡" in tablero[i][j]:
+                str(tablero[i][j]).replace("≡", "1")
+            elif "¤" in tablero[i][j]:
+                str(tablero[i][j]).replace("¤", "2")
 
 def scoreboard():
     path=os.path.dirname(os.path.abspath(__file__))
@@ -123,7 +237,7 @@ def movimiento_barco(direccion,barcos,barco,tablero):
     for i in range(len(barcos[barco])):
         if posible:
             old_x,old_y = barcos[barco][i]
-            tablero[old_x][old_y] = (f"\033[36m ~\033[0m")
+            tablero[old_x][old_y] = "\033[36m ~\033[0m"
             x,y = direccion
             new_x = old_x + x
             new_y = old_y + y
@@ -138,7 +252,7 @@ def movimiento_barco(direccion,barcos,barco,tablero):
 def visualizar_barco(barcos,tablero_barcos):
     for barco in barcos:
         for coordenada in barco:
-            tablero_barcos[coordenada[0]][coordenada[1]] = (f"\033[90m ≡\033[0m")
+            tablero_barcos[coordenada[0]][coordenada[1]] = "\033[90m ≡\033[0m"
             
 def rotacion_a_vertical(barcos,barco,tablero):
     aux = copy.deepcopy(barcos[barco])
@@ -146,7 +260,7 @@ def rotacion_a_vertical(barcos,barco,tablero):
     for coordenada in range(len(barcos[barco])):
         if posible:
             old_x,old_y = barcos[barco][coordenada]
-            tablero[old_x][old_y] = (f"\033[36m ~\033[0m")
+            tablero[old_x][old_y] = "\033[36m ~\033[0m"
             new_x = old_x + coordenada
             new_y = old_y - coordenada
             if new_x < 0 or new_x >= len(tablero)-1 or new_y <= 0 or new_y  >= len(tablero):
@@ -161,7 +275,7 @@ def rotacion_a_horizontal(barcos,barco,tablero):
     for coordenada in range(len(barcos[barco])):
         if posible:
             old_x,old_y = barcos[barco][coordenada]
-            tablero[old_x][old_y] = (f"\033[36m ~\033[0m")
+            tablero[old_x][old_y] = "\033[36m ~\033[0m"
             new_x = old_x - coordenada
             new_y = old_y + coordenada
             if new_x < 0 or new_x >= len(tablero)-1 or new_y <= 0 or new_y  >= len(tablero):
@@ -170,7 +284,7 @@ def rotacion_a_horizontal(barcos,barco,tablero):
             else:
                 barcos[barco][coordenada]=(new_x,new_y)
 
-def confirmar_barco(barcos,barco,partido,arch_tablero):
+def confirmar_barco(barcos,barco,partido,arch_tablero,tablero,tablero_disparo):
     posible = True
     for coordenada in range(len(barcos[barco])):
         if posible:
@@ -182,7 +296,8 @@ def confirmar_barco(barcos,barco,partido,arch_tablero):
     if posible:
         barco+=1
     if barco == 5:
-        print("Creando")
+        convertir_a_numero(tablero_disparo)
+        convertir_a_numero(tablero)
         tableros = json.dumps(partido, indent=4)
         try:
             contenido = open(arch_tablero, "w")
@@ -196,28 +311,14 @@ def confirmar_barco(barcos,barco,partido,arch_tablero):
 
 def visualizar_disparos(disparo,tablero_disparos,bombas):
     for coordenada in bombas:
-        tablero_disparos[coordenada[0]][coordenada[1]] = (f"\033[31m ¤\033[0m")
-    tablero_disparos[disparo[0]][disparo[1]] = (f"\033[37m ¤\033[0m")
-def dibujar_radar(i):
-    o = (f"\033[32m ▓\033[0m") #main
-    p = (f"\033[32m ░\033[0m") #estela 1
-    l= (f"\033[32m ▒\033[0m")  #estela 2
-    m = (f"\033[32m ~\033[0m") #guiones
-    radar0=f"   {m}{p}{m}  \n {m}{m}{l}{l}{m} \n {m}{m}{o}{o}{o} \n {m}{m}{m}{m}{m} \n   {m}{m}{m}"
-    radar1=f"   {m}{m}{m}  \n {m}{m}{p}{p}{m} \n {m}{m}{o}{l}{l} \n {m}{m}{m}{o}{m} \n   {m}{m}{m}"
-    radar2=f"   {m}{m}{m}  \n {m}{m}{m}{m}{m} \n {m}{m}{o}{p}{p} \n {m}{m}{o}{l}{m} \n   {m}{o}{m}"
-    radar3=f"   {m}{m}{m}  \n {m}{m}{m}{m}{m} \n {m}{m}{o}{m}{m} \n {m}{o}{l}{p}{m} \n   {m}{l}{m}"
-    radar4=f"   {m}{m}{m}  \n {m}{m}{m}{m}{m} \n {o}{o}{o}{m}{m} \n {m}{l}{p}{m}{m} \n   {m}{p}{m}"
-    radar5=f"   {m}{m}{m}  \n {m}{o}{m}{m}{m} \n {l}{l}{o}{m}{m} \n {m}{p}{m}{m}{m} \n   {m}{m}{m}"
-    radar6=f"   {m}{o}{m}  \n {m}{l}{o}{m}{m} \n {p}{p}{o}{m}{m} \n {m}{m}{m}{m}{m} \n   {m}{m}{m}"
-    radar7=f"   {m}{l}{m}  \n {m}{p}{l}{o}{m} \n {m}{m}{o}{m}{m} \n {m}{m}{m}{m}{m} \n   {m}{m}{m}"
-    radar = [radar0,radar1,radar2,radar3,radar4,radar5,radar6,radar7]
-    print(radar[i])
+        tablero_disparos[coordenada[0]][coordenada[1]] = "\033[31m ¤\033[0m"
+    tablero_disparos[disparo[0]][disparo[1]] = "\033[37m ¤\033[0m"
+
 def movimiento_disparo(direccion,bomba,tablero):
     posible = True
     if posible:
         old_x,old_y = bomba
-        tablero[old_x][old_y] = (f"\033[36m ~\033[0m")
+        tablero[old_x][old_y] = "\033[36m ~\033[0m"
         x,y = direccion
         new_x = old_x + x
         new_y = old_y + y
@@ -227,17 +328,14 @@ def movimiento_disparo(direccion,bomba,tablero):
             bomba = (new_x,new_y)
     return bomba
 
-def confirmar_tiro(posicion_tiro,tiros,confirmable, partido):
+def confirmar_tiro(posicion_tiro,tiros,confirmable):
 #TODO el primer parámetro tiene que ser el nombre que le demos a la posición actual del disparo
     confirmable = True
     if tiros == []:
-        print("1")
         return confirmable
     else:
         for tiro in range(len(tiros)):
-            print("2")
             if tiros[tiro] == posicion_tiro:
-                print("3")
                 confirmable = False
         return confirmable
        
@@ -250,10 +348,9 @@ def deteccion_disparo(disparo, lista_barcos):
             if disparo == lista_barcos[i][j] and encontrado == False:
                 del lista_barcos[i][j]
                 encontrado = True
-                if not lista_barcos[i]:
-                    del lista_barcos[i]
             j += 1
         i += 1
+    return encontrado
 
 def juego():
     cursor.hide()
@@ -261,8 +358,8 @@ def juego():
     clock = pygame.time.Clock()
     path_tableros = os.path.dirname(os.path.abspath(__file__))
     arch_tab = os.path.join(path_tableros, f"Tableros.json")
-    o = (f"\033[36m ~\033[0m")
-    b = (f"\033[90m ≡\033[0m")
+    o = "\033[36m ~\033[0m"
+    b = "\033[90m ≡\033[0m"
     portaaviones = b
     mulportaaviones = 5
     destructor = b
@@ -332,7 +429,6 @@ def juego():
     tirosj1 = []
     tirosj2 = []
     pos_bomba = (1,1)
-    radar = 0
     turno = 1
     miturno = 1
     game = True
@@ -356,30 +452,30 @@ def juego():
             if keyboard.is_pressed('w'):
                 if presionado == False:
                     if estado == "posicionar barcos":
-                        movimiento_barco((-1,0),barcosj1,num_barco,partida["Jugador 1"]["tablero barcos"])
+                        movimiento_barco((-1,0),barcosj1,num_barco,j1_tablerobarcos)
                     elif estado == "posicionar disparos":
-                        pos_bomba = movimiento_disparo((-1,0),pos_bomba,partida["Jugador 1"]["tablero disparos"])
+                        pos_bomba = movimiento_disparo((-1,0),pos_bomba,j1_tablerodisparos)
                 presionado = True
             elif keyboard.is_pressed('s'):
                 if presionado == False:
                     if estado == "posicionar barcos":
-                        movimiento_barco((1,0),barcosj1,num_barco,partida["Jugador 1"]["tablero barcos"])
+                        movimiento_barco((1,0),barcosj1,num_barco,j1_tablerobarcos)
                     elif estado == "posicionar disparos":
-                        pos_bomba = movimiento_disparo((1,0),pos_bomba,partida["Jugador 1"]["tablero disparos"])
+                        pos_bomba = movimiento_disparo((1,0),pos_bomba,j1_tablerodisparos)
                 presionado = True
             elif keyboard.is_pressed('d'):
                 if presionado == False:
                     if estado == "posicionar barcos":
-                        movimiento_barco((0,1),barcosj1,num_barco,partida["Jugador 1"]["tablero barcos"])
+                        movimiento_barco((0,1),barcosj1,num_barco,j1_tablerobarcos)
                     elif estado == "posicionar disparos":
-                        pos_bomba = movimiento_disparo((0,1),pos_bomba,partida["Jugador 1"]["tablero disparos"])
+                        pos_bomba = movimiento_disparo((0,1),pos_bomba,j1_tablerodisparos)
                 presionado = True
             elif keyboard.is_pressed('a'):
                 if presionado == False:
                     if estado == "posicionar barcos":
-                        movimiento_barco((0,-1),barcosj1,num_barco,partida["Jugador 1"]["tablero barcos"])
+                        movimiento_barco((0,-1),barcosj1,num_barco,j1_tablerobarcos)
                     elif estado == "posicionar disparos":
-                        pos_bomba = movimiento_disparo((0,-1),pos_bomba,partida["Jugador 1"]["tablero disparos"])
+                        pos_bomba = movimiento_disparo((0,-1),pos_bomba,j1_tablerodisparos)
                 presionado = True
             elif keyboard.is_pressed('r'):
                 if presionado == False:
@@ -389,17 +485,30 @@ def juego():
                         else:
                             rotacion_a_horizontal(barcosj1,num_barco,j1_tablerobarcos)#TODO cuando se pone la direccion con el diccionario se rompe
                 presionado = True
-            elif keyboard.is_pressed('enter'):
+            elif keyboard.is_pressed('e'):
                 if presionado == False:
                     if estado == "posicionar barcos":
-                        num_barco = confirmar_barco(barcosj1,num_barco, partida, arch_tab)
+                        num_barco = confirmar_barco(barcosj1,num_barco, partida, arch_tab,j1_tablerobarcos,j1_tablerodisparos)
                     elif estado =="posicionar disparos":
-                        confirmado = confirmar_tiro(pos_bomba,tirosj1,confirmado, partida, arch_tab)
-                        print("6")
+                        confirmado = confirmar_tiro(pos_bomba,tirosj1,confirmado)
                         if confirmado:
-                            print("7")
+                            encontrar = deteccion_disparo(pos_bomba, barcosj1)
+                            if encontrar:
+                                print("TOCADO!")
+                            elif not encontrar:
+                                print("AGUA!")
                             tirosj1.append(pos_bomba)
-                            print("8")
+                            convertir_a_numero(j1_tablerobarcos)
+                            convertir_a_numero(j1_tablerodisparos)
+                            tableros = json.dumps(partida, indent=4)
+                            try:
+                                contenido = open(arch_tab, "w")
+                                contenido.write(tableros)
+                                contenido.close()
+                                print("crado")
+                            except TypeError:
+                                print(TypeError)
+                                print("error de grabado de cambios")
                             pos_bomba = (1,1)
                 presionado = True
             else:
@@ -407,16 +516,12 @@ def juego():
         else:
             print("Esperando oponente...")
         sys.stdin.flush()
-        visualizar_barco(barcosj1,partida["Jugador 1"]["tablero barcos"])
+        visualizar_barco(barcosj1,j1_tablerobarcos)
         visualizar_disparos(pos_bomba,j1_tablerodisparos,tirosj1)
         dibujar(j1_tablerodisparos)
-        dibujar(partida["Jugador 1"]["tablero barcos"])
+        dibujar(j1_tablerobarcos)
         print(tirosj1)
         clock.tick(24)
-        dibujar_radar(int(radar))
-        radar +=0.1
-        if radar >= 8:
-            radar = 0
         os.system("cls")
 equipo = ["Diaz, German Ezequiel", "Nuñez Gagliano, Francisco Dario", "Ragagnin, Nicolas",
           "Salas, Agustin Ezequiel", "Sandoval, Marianella Jazmín", "Trimarco, Tomas","McLovin"]
