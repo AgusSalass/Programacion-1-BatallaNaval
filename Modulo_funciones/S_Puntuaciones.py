cedd70d176390f7d721d0702b89bddf3caa48e28
Trim-Ai <trimitf9@gmail.com>
2024-11-07 14:41:44 -0300

    1Version_puntuaciones

--- Modulo_funciones/S_Puntuaciones.py
+++ Modulo_funciones/S_Puntuaciones.py
@@ -1,15 +1,29 @@
 import json
 import os
 
-path=os.path.dirname(os.path.abspath(__file__))
-arch_div = os.path.join(path, f"Usuarios.json")
-tablas=open(arch_div, "r")
-contenido_tablas=json.load(tablas)
-tablas.close()
+
+def leer_archivo():
+	try:
+		path_archivo = os.path.dirname(os.path.abspath(__file__))
+		arch_dic = os.path.join(path_archivo, f"Usuarios.json")
+		contenido = open(arch_dic,"r")
+		cont_existente = contenido.read()
+		contenido.close()
+		diccionario_usuarios = json.loads(cont_existente)
+		return diccionario_usuarios
+	except TypeError:
+		print(TypeError)
+		print("Error de lectura")
+
+usuarios = leer_archivo()
+cuenta=("12345")
 tuplas_usuarios=[]
-for usuario in contenido_tablas:
-    puntaje=contenido_tablas[usuario]["puntaje"]
-    tuplas_usuarios.append((puntaje,usuario))
-print(tuplas_usuarios)
-tuplas_o=tuple(sorted(tuplas_usuarios,reverse=True))
-print(tuplas_o)
+print("\n\t\t\tTABLERO DE PUNTUACIONES")
+print("\t\t\t-------------------------")
+
+puntaje=usuarios[cuenta]["puntaje"]
+tuplas_usuarios.append((puntaje,cuenta))
+tuplas_o=sorted(tuplas_usuarios,reverse=True)
+for puntaje,usuario in tuplas_o:
+	print(f"\t\t\t{usuario}: {puntaje}")
+
